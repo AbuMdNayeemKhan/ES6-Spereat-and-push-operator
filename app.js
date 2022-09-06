7b91464243889bb0a1e75f48f3a7b75ce8e81eb6
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-09-06 11:12:18 +0600

    Spereat and push Operator

---
+++ app.js
@@ -0,0 +1,11 @@
+//==========Spereat Operator==========//
+// Sperate Operator for use concatenation  arrays 
+let boys = ["Nayeem", "Zioun", "Tishat", "Rohan", "Rafi"];
+let girls = ["Shatul", "Meghla", "Anika"];
+// let both =[...boys, ...girls];
+
+//==========Push Operator==========//
+// You also concatenation array by using push method but it's create a associative array.
+let bothPush = boys.push(girls);
+
+console.log(bothPush);
